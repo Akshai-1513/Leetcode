4f932fa0685ccae602d079baeeb0e128d45b8809
Akshai Kumar TN <akshaikumartn@gmail.com>
2025-02-03 17:28:47 +0000

    Sync LeetCode submission Runtime - 5 ms (74.08%), Memory - 44.2 MB (99.97%)

--- 0009-palindrome-number/solution.java
+++ 0009-palindrome-number/solution.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String str = String.valueOf(x);
-        String rev ="";
-        for (int i = 0; i < str.length();i++){
-            rev = str.charAt(i) + rev; 
-        }
-        if (str.equals(rev))
+        int num = x;
+        int rev = 0;
+        while(num > 0){
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num = num / 10;
+        }        
+        if (x == rev)
             return true;
         else
             return false;
-            
     }
 }
