68f82edb2cde74f01e5507911de27a13ce9432e8
Akshai-1513 <akshaikumartn@gmail.com>
2025-06-16 22:43:07 +0530

    The execued succcessfully

--- Binary_addition.java
+++ Binary_addition.java
@@ -1,14 +1,14 @@
 /*
 Given two binary strings a and b, return their sum as a binary string.
 
-Example 1:
+    Example 1:
 
-Input: a = "11", b = "1"
-Output: "100"
-Example 2:
+    Input: a = "11", b = "1"
+    Output: "100"
+    Example 2:
 
-Input: a = "1010", b = "1011"
-Output: "10101"
+    Input: a = "1010", b = "1011"
+    Output: "10101"
 
  */
 
@@ -31,7 +31,6 @@ public class Binary_addition {
         }
         return res.reverse().toString();
     }
-
 }
 
 
