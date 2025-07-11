73a6239b01e99b31ca9e25827d4a4f0e9e995d5e
Akshai Kumar TN <akshaikumartn@gmail.com>
2025-07-11 17:40:38 +0000

    Sync LeetCode submission Runtime - 3 ms (98.05%), Memory - 44.4 MB (99.92%)

---
+++ 1486-find-the-distance-value-between-two-arrays/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int count = 0;
+        for(int i : arr1){
+            boolean flag = false;
+            for(int j : arr2){
+                if(Math.abs(i - j) <= d){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag == false) count++;
+        }
+       return count;
+    }
+}
