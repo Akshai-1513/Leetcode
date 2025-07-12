40ba8498c3ee10925447198c82b2923860bd6e08
Akshai-1513 <akshaikumartn@gmail.com>
2025-07-12 21:15:18 +0530

    The file name is changed

---
+++ Searching/Binary_Search/prob_1385_find_dist_val.java
@@ -0,0 +1,4 @@
+package Searching.Binary_Search;
+
+public class prob_1385_find_dist_val {
+}
--- prob_1_Two_sum.java
+++ prob_1_Two_sum.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Two_sum {
+public class prob_1_Two_sum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer , Integer> map = new HashMap<>();
         for(int i = 0; i< nums.length; i++ ){
