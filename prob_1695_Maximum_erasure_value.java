2f460ce719d91519ac1af500614ac30753a8ae3e
Akshai-1513 <akshaikumartn@gmail.com>
2025-07-25 00:27:29 +0530

    The program is to find the all possible divisors then sum it, if the sum is equal to the num the it is perfecct number.

---
+++ prob_1695_Maximum_erasure_value.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class prob_1695_Maximum_erasure_value {
+}
---
+++ prob_507_perfect_number.java
@@ -0,0 +1,53 @@
+/*
+507. Perfect Number
+    A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+    Given an integer n, return true if n is a perfect number, otherwise return false.
+
+Example 1:
+    Input: num = 28
+    Output: true
+    Explanation: 28 = 1 + 2 + 4 + 7 + 14
+    1, 2, 4, 7, and 14 are all divisors of 28.
+
+Example 2:
+    Input: num = 7
+    Output: false
+
+Constraints:
+    1 <= num <= 108
+ */
+
+
+/*
+    This approach is like
+    num = 28;
+    1 * 28
+    2 * 14
+    4 * 7
+ */
+public class prob_507_perfect_number {
+    public static boolean checkPerfectNumber(int num) {
+        int sum = 1;
+        if(num == 1) return true;
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0) {
+                if(num / i == i) {
+                    sum += i;
+                    System.out.println(sum);
+                }
+                else {
+                    sum += i + (num / i);
+                    System.out.println(sum);
+                }
+            }
+        }
+        if(sum == num) return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int num = 28;
+        if(checkPerfectNumber(num)) System.out.println(true);
+        else System.out.println(false);
+    }
+}
