29a84c1fc871457c21961bda94cc668f05f76e25
Akshai-1513 <akshaikumartn@gmail.com>
2025-08-29 22:39:09 +0530

    The problem is to find whether we can create the ransomnote using the magnize string.

---
+++ prob_383_ransomnote.java
@@ -0,0 +1,63 @@
+/*
+383. Ransom Note
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+
+
+Constraints:
+
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters.
+
+ */
+
+public class prob_383_ransomnote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char[] ram = toarray(ransomNote);
+        char[] mag = toarray(magazine);
+
+        for(int i = 0; i < mag.length; i++){
+            for(int j = 0; j < ram.length; j++){
+                if(mag[i] == ram[j]){
+                    mag[i] = '\0';
+                    ram[j] = '\0';
+                }
+            }
+        }
+
+        boolean flag = true;
+        for(char i : ram){
+            if(i != '\0') return false;
+        }
+        return true;
+    }
+
+    public char[] toarray(String str){
+        char[] arr = new char[str.length()];
+        for(int i = 0; i < str.length(); i++){
+            arr[i] = str.charAt(i);
+        }
+        return arr;
+    }
+}
