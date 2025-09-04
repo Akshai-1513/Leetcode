f33177a164ee3f5348929596c1c2cecb91966cbd
Akshai Kumar TN <akshaikumartn@gmail.com>
2025-09-04 02:07:09 +0000

    Sync LeetCode submission Runtime - 1 ms (93.07%), Memory - 41.6 MB (99.96%)

--- 3830-find-closest-person/solution.java
+++ 3830-find-closest-person/solution.java
@@ -1,9 +1,8 @@
 class Solution {
     public int findClosest(int x, int y, int z) {
-        int x_dist = Math.abs(z - x);
-        int y_dist = Math.abs(z - y);
-        if(x_dist < y_dist) return 1;
-        if(x_dist > y_dist) return 2;
-        return 0; 
+        int p1 = Math.abs(x - z), p2 = Math.abs(y - z);
+        if(p1 == p2) return 0;
+        if(p1 < p2) return 1;
+        return 2;
     }
 }
