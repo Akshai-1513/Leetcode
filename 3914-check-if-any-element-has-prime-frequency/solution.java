3438cacafd0b8ffade762ad0b8f8c7fe5e8f97ad
Akshai Kumar TN <akshaikumartn@gmail.com>
2025-09-08 21:36:15 +0000

    Sync LeetCode submission Runtime - 1 ms (93.70%), Memory - 42.2 MB (100.00%)

--- 3914-check-if-any-element-has-prime-frequency/solution.java
+++ 3914-check-if-any-element-has-prime-frequency/solution.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean checkPrimeFrequency(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i : nums) map.put(i, map.getOrDefault(i, 0) + 1);
-        for(int i : map.values()){
+        int max = nums[0];
+        for(int i : nums) max = (i > max) ? i : max;
+        int[] freq = new int[max + 1];
+        for(int i : nums){
+            freq[i]++;
+        }
+
+        for(int i : freq) {
             if(isPrime(i)) return true;
         }
         return false;
     }
-
     public boolean isPrime(int n){
         if(n < 2) return false;
         for(int i = 2; i <= Math.sqrt(n); i++) if(n % i == 0) return false;
