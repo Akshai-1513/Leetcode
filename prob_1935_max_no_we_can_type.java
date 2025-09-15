38c9c1fc075b8c82e895e62d3769b44a96c697e6
Akshai-1513 <akshaikumartn@gmail.com>
2025-09-15 22:20:42 +0530

    The problem is to type the words, but the user has broken letters those letters can't be used

---
+++ prob_1935_max_no_we_can_type.java
@@ -0,0 +1,58 @@
+/*
+1935. Maximum Number of Words You Can Type
+    There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+    Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
+
+Example 1:
+    Input: text = "hello world", brokenLetters = "ad"
+    Output: 1
+    Explanation: We cannot type "world" because the 'd' key is broken.
+
+Example 2:
+    Input: text = "leet code", brokenLetters = "lt"
+    Output: 1
+    Explanation: We cannot type "leet" because the 'l' and 't' keys are broken.
+
+Example 3:
+    Input: text = "leet code", brokenLetters = "e"
+    Output: 0
+    Explanation: We cannot type either word because the 'e' key is broken.
+
+Constraints:
+    1 <= text.length <= 104
+    0 <= brokenLetters.length <= 26
+    text consists of words separated by a single space without any leading or trailing spaces.
+    Each word only consists of lowercase English letters.
+    brokenLetters consists of distinct lowercase English letters.
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class prob_1935_max_no_we_can_type {
+    static int canBeTypedWords(String text, String brokenLetters) {
+        int count = 0;
+        String[] words = text.split(" ");
+
+        Set<Character> set = new HashSet<>();
+        for (char c : brokenLetters.toCharArray()) set.add(c);
+
+        for (String i : words) {
+            boolean flag =  true;
+            for(char c : i.toCharArray()) {
+                if(set.contains(c)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String text = "hello world";
+        String btext = "ad";
+        System.out.println(canBeTypedWords(text,btext));
+    }
+}
