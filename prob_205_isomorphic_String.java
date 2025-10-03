b5d93eb8f038d7fe52a206d08cb9e5d9f1630833
Akshai-1513 <akshaikumartn@gmail.com>
2025-10-04 00:34:22 +0530

    The problem is to find whether the two strings are isomophic or not

---
+++ prob_205_isomorphic_String.java
@@ -0,0 +1,64 @@
+/*
+205. Isomorphic Strings
+    Given two strings s and t, determine if they are isomorphic.
+    Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+    All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+Example 1:
+    Input: s = "egg", t = "add"
+    Output: true
+    Explanation:
+        The strings s and t can be made identical by:
+            Mapping 'e' to 'a'.
+            Mapping 'g' to 'd'.
+
+Example 2:
+    Input: s = "foo", t = "bar"
+    Output: false
+    Explanation:
+        The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+
+Example 3:
+    Input: s = "paper", t = "title"
+    Output: true
+
+Constraints:
+    1 <= s.length <= 5 * 104
+    t.length == s.length
+    s and t consist of any valid ascii character.
+ */
+import java.util.*;
+public class prob_205_isomorphic_String {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> map1 = maps(s, t);
+        HashMap<Character, Character> map2 = maps(t, s);
+
+        for(int i = 0; i < s.length(); i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+
+            if(map1.containsKey(ch1)) {
+                if(map1.get(ch1) != ch2) return false;
+                else map1.put(ch1, ch2);
+            }
+
+            if(map2.containsKey(ch2)) {
+                if(map2.get(ch2) != ch1) return false;
+                else map2.put(ch2, ch1);
+            }
+        }
+        return true;
+    }
+
+
+    private HashMap<Character, Character> maps(String s, String t){
+        HashMap<Character, Character> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            if(!map.containsKey(ch1)) map.put(ch1, ch2);
+        }
+
+        return map;
+    }
+}
