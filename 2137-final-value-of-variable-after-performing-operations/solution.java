975c5c9aa66677aaaf4b01773f1d765ba45f2825
Akshai Kumar TN <akshaikumartn@gmail.com>
2025-10-20 08:57:27 +0000

    Sync LeetCode submission Runtime - 1 ms (78.62%), Memory - 42.6 MB (99.95%)

---
+++ 2137-final-value-of-variable-after-performing-operations/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int res = 0;
+        for(String i : operations){
+            if(i.equals("X++")) res++;
+            else if(i.equals("++X")) ++res;
+            else if(i.equals("X--")) res--;
+            else --res;
+        }
+        return res;
+    }
+}
