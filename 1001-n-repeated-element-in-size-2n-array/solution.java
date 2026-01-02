d8a7e61fc8ab3d99f7d345d67e4e1abfaa09eec4
Akshai Kumar TN <akshaikumartn@gmail.com>
2026-01-02 08:55:23 +0000

    Sync LeetCode submission Runtime - 17 ms (21.37%), Memory - 47.4 MB (73.33%)

---
+++ 1001-n-repeated-element-in-size-2n-array/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length / 2;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : nums) map.put(i, map.getOrDefault(i, 0) + 1);
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue() == n) return entry.getKey();
+        } 
+        return -1;
+    }
+}
