b344ea13c656e2450c118466af4a7dea112424d3
Akshai Kumar TN <akshaikumartn@gmail.com>
2026-02-17 17:25:06 +0000

    Sync LeetCode submission Runtime - 5 ms (83.74%), Memory - 49.2 MB (53.77%)

--- 0401-binary-watch/solution.java
+++ 0401-binary-watch/solution.java
@@ -1,6 +1,7 @@
 class Solution {
     public List<String> readBinaryWatch(int turnedOn) {
         List<String> res = new ArrayList<>();
+        if(turnedOn > 8) return res;
         for(int h = 0; h < 12; h++){
             for(int m = 0; m < 60; m++){
                 if(Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
