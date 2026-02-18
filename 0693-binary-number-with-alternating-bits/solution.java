561f5eadc0d50b52b41016fb9a975d3cfabceb0f
Akshai Kumar TN <akshaikumartn@gmail.com>
2026-02-18 19:03:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (75.14%)

--- 0693-binary-number-with-alternating-bits/solution.java
+++ 0693-binary-number-with-alternating-bits/solution.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        StringBuilder str = new StringBuilder();
-        while(n > 0){
-            str.append(n % 2);
-            n /= 2;
-        }
+        String str = Integer.toBinaryString(n);
 
-        char val = str.charAt(0);
         for(int i = 1; i < str.length(); i++){
-            char ch = str.charAt(i);
-            if(val == ch) return false;
-            val = ch;
+            if(str.charAt(i) == str.charAt(i - 1)) return false;
         }
 
         return true;
