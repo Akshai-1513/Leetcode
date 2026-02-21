2f3ba555b62db5aab583d587c54e16d70b966a9c
Akshai Kumar TN <akshaikumartn@gmail.com>
2026-02-21 17:48:57 +0000

    Sync LeetCode submission Runtime - 6 ms (54.28%), Memory - 42.4 MB (64.06%)

--- 0767-prime-number-of-set-bits-in-binary-representation/solution.java
+++ 0767-prime-number-of-set-bits-in-binary-representation/solution.java
@@ -2,25 +2,17 @@ class Solution {
     public int countPrimeSetBits(int left, int right) {
         int count = 0;
         for(int i = left; i <= right; i++){
-            int num = i;
-            int n = 0;
-            while (num > 0){
-                int rem = num % 2;
-                n += rem;
-                num /= 2;
-            }
-            
-            if(isPrime(n)) count++;
-            
+            int val = Integer.bitCount(i);
+            if(isPrime(val)) count++;
         }
         return count;
     }
 
-    private boolean isPrime(int n){
-        if(n == 0 || n == 1) return false;
-        for(int i = 2; i <= Math.sqrt(n); i++){
-        if(n % i == 0) return false;
-           }
+    boolean isPrime(int n) {
+        if(n == 1 || n == 0) return false;
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) return false;
+        }
         return true;
     }
 }
