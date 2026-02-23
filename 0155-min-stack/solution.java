8d41f33fe0cbbed0a557ae60f746306f560db2a7
Akshai Kumar TN <akshaikumartn@gmail.com>
2026-02-23 11:01:40 +0000

    Sync LeetCode submission Runtime - 316 ms (5.05%), Memory - 47 MB (68.15%)

---
+++ 0155-min-stack/solution.java
@@ -0,0 +1,34 @@
+class MinStack {
+    List<Integer> list;
+
+    public MinStack() {
+        list = new ArrayList<>();
+    }
+    
+    public void push(int val) {
+        list.add(val);
+    }
+    
+    public void pop() {
+        list.remove(list.size() - 1);
+    }
+    
+    public int top() {
+        return list.get(list.size() - 1);
+    }
+    
+    public int getMin() {
+        int val = list.get(0);
+        for(int i : list) val = val < i ? val : i;
+        return val;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
